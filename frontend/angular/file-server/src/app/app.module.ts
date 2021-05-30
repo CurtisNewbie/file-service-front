540b6812d242f7e2f01b8ab1b504957583a794dc
CurtisNewbie <zhuangyongj@gmail.com>
2021-05-31 00:10:59 +0800

    Init home-page component

--- frontend/angular/file-server/src/app/app.module.ts
+++ frontend/angular/file-server/src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { APP_BASE_HREF } from "@angular/common";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomePageComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
