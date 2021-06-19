51e81eca29caf9b9f62b8901cb08decb04da0834
CurtisNewbie <zhuangyongj@gmail.com>
2021-06-19 17:22:14 +0800

    Extract code for controlling pagination

--- frontend/angular/file-server-front/src/models/paging.ts
+++ frontend/angular/file-server-front/src/models/paging.ts
@@ -7,3 +7,58 @@ export interface Paging {
   /** total number of items */
   total: number;
 }
+
+/**
+ * Constants related paging
+ */
+export class PagingConst {
+  /** Get default paging limit options */
+  public static getPagingLimitOptions(): number[] {
+    return [5, 10, 20, 50];
+  }
+}
+
+/**
+ * Controller for pagination, internal properties are non-private, thus can be directly bound with directive
+ */
+export class PagingController {
+  readonly PAGE_LIMIT_OPTIONS: number[] = PagingConst.getPagingLimitOptions();
+  paging: Paging = {
+    page: 1,
+    limit: this.PAGE_LIMIT_OPTIONS[0],
+    total: 0,
+  };
+  pages: number[] = [1];
+
+  /** Update the list of pages that it can select */
+  public updatePages(total: number): void {
+    this.pages = [];
+    let maxPage = Math.ceil(total / this.paging.limit);
+    for (let i = 1; i <= maxPage; i++) {
+      this.pages.push(i);
+    }
+    if (this.pages.length === 0) {
+      this.pages.push(1);
+    }
+  }
+
+  /** Set page number */
+  public setPage(page: number): void {
+    this.paging.page = page;
+  }
+
+  /** Set Page limit */
+  public setPageLimit(limit: number): void {
+    this.paging.limit = limit;
+  }
+
+  /** Whether it can go to the next page */
+  public canGoToNextPage(): boolean {
+    return this.paging.page < this.pages[this.pages.length - 1];
+  }
+
+  /** Whether it can go to the prevous page */
+  public canGoToPrevPage(): boolean {
+    return this.paging.page > 1;
+  }
+}
