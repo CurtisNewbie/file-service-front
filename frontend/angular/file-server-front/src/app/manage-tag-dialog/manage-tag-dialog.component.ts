7f85fc397dfad8eb62bf9adf018ea154e3ef99ac
yongj.zhuang <zhuangyongj@gmail.com>
2022-06-04 02:12:02 +0800

    Fix bug

--- frontend/angular/file-server-front/src/app/manage-tag-dialog/manage-tag-dialog.component.ts
+++ frontend/angular/file-server-front/src/app/manage-tag-dialog/manage-tag-dialog.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject, OnInit } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { PageEvent } from "@angular/material/paginator";
 import { Tag } from "src/models/file-info";
-import { Paging, PagingController } from "src/models/paging";
+import { PagingController } from "src/models/paging";
 import { FileInfoService } from "../file-info.service";
 import { NotificationService } from "../notification.service";
 
