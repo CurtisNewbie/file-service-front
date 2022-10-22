d9eeefe67d8d444531e6f30366aa87c0677aa088
yongj.zhuang <zhuangyongj@gmail.com>
2022-10-23 01:49:16 +0800

    Fix export

--- frontend/angular/file-server-front/src/app/util/env-util.ts
+++ frontend/angular/file-server-front/src/app/util/env-util.ts
@@ -1,3 +1,9 @@
+import { environment } from "src/environments/environment";
+
 export function isMobile(): boolean {
   return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+}
+
+export function isServiceEnabled(service: string): boolean {
+  return environment.services.find((v) => v.base === service) != null;
 }
